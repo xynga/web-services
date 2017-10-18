3f8f2f33b8ac26acd522db13898e5128071eaa1a
jaronson <aronson_jesse@wheatoncollege.edu>
2017-10-18 13:29:10 -0400

    initial commit

---
+++ service.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { WebserviceService } from './services/webservice.service';
+import { ApiService } from "./services/api.service";
+import { HttpModule } from '@angular/http';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule
+
+  ],
+  providers: [
+    WebserviceService,
+    ApiService
+  ],
+  declarations: [],
+  exports: []
+})
+export class ServiceModule {}
