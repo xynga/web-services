b268c56d92322dad58776d72c5a693b38e29916a
jaronson <aronson_jesse@wheatoncollege.edu>
2017-10-19 17:21:38 -0400

    Add services back into index

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export {ServiceModule} from './services/service.module';
+export {ApiService} from './services/api.service';
+export {IdleService} from './services/idle.service';
+export {WebService} from './services/web.service';
