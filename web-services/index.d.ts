ec75be45fbc83cfb21f5821ed02a6f259826ef3a
jaronson <aronson_jesse@wheatoncollege.edu>
2018-01-03 11:45:46 -0500

    initial restructuring

--- web-services/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ServiceModule } from './services/service.module';
-export { ApiService } from './services/api.service';
-export { IdleService } from './services/idle.service';
-export { WebService } from './services/web.service';
-export { CanonicalUser, CanonicalUserResponse, Credentials, UpdateCanonicalUserRequest } from './models/canonical-user.model';
